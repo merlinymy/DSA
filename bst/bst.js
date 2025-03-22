4e2d35c9a9aaeb4fca04dc457d91278331e522a4
merlinymy <lordhorcrux@gmail.com>
2025-03-21 21:02:32 -0500

    add remaining features

--- bst/bst.js
+++ bst/bst.js
@@ -19,7 +19,7 @@ class Tree {
       return false;
     }
     if (val === node.data) {
-      return true;
+      return node;
     }
     if (val < node.data) {
       return this.find(val, node.left);
@@ -28,11 +28,94 @@ class Tree {
     }
   }
 
+  rebalanceTree() {
+    let newArray = [];
+    this.inOrder((node, arr = newArray) => {
+      arr.push(node.data);
+    });
+    this.root = this.buildTree(newArray);
+  }
+
+  depth(node, roo, depth = 0) {
+    // the number of edges from the root to the node.
+    // console.log(`node is ${node.data}`);
+    if (roo === null) {
+      return 0;
+    }
+    if (node.data === roo.data) {
+      return depth;
+    }
+
+    let res = this.depth(node, roo.left, depth + 1);
+    if (res === 0) {
+      res = this.depth(node, roo.right, depth + 1);
+    }
+    return res;
+  }
+
+  height(target) {
+    if (target === null) return -1;
+    let leftHeight = this.height(target.left);
+    let rightHeigt = this.height(target.right);
+    return Math.max(leftHeight, rightHeigt) + 1;
+  }
+
+  isBalanced(root) {
+    if (root === null) return true;
+    let leftHeight = tree.height(root.left);
+    let rightHeight = tree.height(root.right);
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      return false;
+    }
+    return this.isBalanced(root.left) && this.isBalanced(root.right);
+  }
+
+  levelOrder(callback, queue = [this.root]) {
+    if (queue.length === 0) {
+      return;
+    }
+    let curr = queue.shift();
+    callback(curr);
+    if (curr.left) queue.push(curr.left);
+    if (curr.right) queue.push(curr.right);
+    this.levelOrder(callback, queue);
+  }
+
+  inOrder(callback, node = this.root) {
+    // left root right
+    if (node === null) {
+      return;
+    }
+    this.inOrder(callback, node.left);
+    callback(node);
+    this.inOrder(callback, node.right);
+  }
+
+  preOrder(callback, node = this.root) {
+    // root left right
+    if (node === null) {
+      return;
+    }
+    callback(node);
+    this.preOrder(callback, node.left);
+    this.preOrder(callback, node.right);
+  }
+
+  postOrder(callback, node = this.root) {
+    //left right root
+    if (node === null) {
+      return;
+    }
+    this.postOrder(callback, node.left);
+    this.postOrder(callback, node.right);
+    callback(node);
+  }
+
   insert(val) {
     if (this.root === null) {
-      this.root = new Node(val);
+      this.root = new Node(+val);
     } else {
-      this.insertHelper(this.root, val);
+      this.insertHelper(this.root, +val);
     }
   }
 
